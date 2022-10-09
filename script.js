10c9207eaede45d143fd723543a7283701114719
CRYS <mariano.christopher123@gmail.com>
2022-10-09 15:14:17 +0800

    edit script to fix input errors

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ window.addEventListener(`load`, function () {
   class InputHandler {
     constructor(game) {
       this.game = game;
-      window.addEventListener(`keydown`, (e) => {
+      window.addEventListener("keydown", (e) => {
         if (
-          (e.key === `ArrowUp` || e.key === `ArrowDown`) &&
-          this.game.keys.indexOf(e.keys) === -1
+          (e.key === "ArrowUp" || e.key === `ArrowDown`) &&
+          this.game.keys.indexOf(e.key) === -1
         ) {
           this.game.keys.push(e.key);
         } else if (e.key === " ") {
