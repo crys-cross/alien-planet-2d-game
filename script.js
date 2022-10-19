f963aa8d4f0c2339cde2087346fcfec74db3407e
Crys <mariano.christopher123@gmail.com>
2022-10-19 12:50:16 +0800

    edit script to add powerUp effects

--- script.js
+++ script.js
@@ -68,8 +68,11 @@ window.addEventListener(`load`, function () {
       this.maxSpeed = 3;
       this.projectiles = [];
       this.image = document.getElementById("player");
+      this.powerUp = false;
+      this.powerUpTimer = 0;
+      this.powerUpLimit = 10000;
     }
-    update() {
+    update(deltaTime) {
       if (this.game.keys.includes(`ArrowUp`)) this.speedY = -this.maxSpeed;
       else if (this.game.keys.includes(`ArrowDown`))
         this.speedY = this.maxSpeed;
@@ -88,10 +91,25 @@ window.addEventListener(`load`, function () {
       } else {
         this.frameX = 0;
       }
+      // power up
+      if (this.powerUp) {
+        if (this.powerUpTimer > this.powerUpLimit) {
+          this.powerUpTimer = 0;
+          this.powerUp = false;
+          this.frameY = 0;
+        } else {
+          this.powerUpTimer += deltaTime;
+          this.frameY = 1;
+          this.game.ammo += 0.1;
+        }
+      }
     }
     draw(context) {
       if (this.game.debug)
         context.strokeRect(this.x, this.y, this.width, this.height);
+      this.projectiles.forEach((projectiles) => {
+        projectiles.draw(context);
+      });
       context.drawImage(
         this.image,
         this.frameX * this.width,
@@ -103,9 +121,6 @@ window.addEventListener(`load`, function () {
         this.width,
         this.height
       );
-      this.projectiles.forEach((projectiles) => {
-        projectiles.draw(context);
-      });
     }
     shootTop() {
       if (this.game.ammo > 0) {
@@ -114,6 +129,20 @@ window.addEventListener(`load`, function () {
         );
         this.game.ammo--;
       }
+    }
+    shootButtom() {
+      if (this.game.ammo > 0) {
+        this.projectiles.push(
+          new Projectile(this.game, this.x + 80, this.y + 175)
+        );
+        this.game.ammo--;
+      }
+      if (this.powerUp) this.shootButtom();
+    }
+    enterPowerUp() {
+      this.powerUpTimer = 0;
+      this.powerUp = true;
+      this.game.ammo = this.game.maxAmmo;
     }
   }
   // main blueprint handling many different enemy types
@@ -257,11 +286,6 @@ window.addEventListener(`load`, function () {
       context.font = this.fontSize + "px" + this.fontFamily;
       // score
       context.fillText("Score: " + this.game.score, 20, 40);
-      // ammo
-
-      for (let i = 0; i < this.game.ammo; i++) {
-        context.fillRect(20 + 5 * i, 50, 3, 20);
-      }
       //timer
       const formattedTime = (this.game.gameTime * 0.001).toFixed(1);
       context.fillText("timer: " + formattedTime, 20, 100);
@@ -289,6 +313,11 @@ window.addEventListener(`load`, function () {
           this.game.width + 0.5,
           this.game.height + 0.5 + 40
         );
+      }
+      // ammo
+      if (this.game.player.powerUp) context.fillStyle = "#ffffbd";
+      for (let i = 0; i < this.game.ammo; i++) {
+        context.fillRect(20 + 5 * i, 50, 3, 20);
       }
       context.restore();
     }
@@ -323,7 +352,7 @@ window.addEventListener(`load`, function () {
       if (this.gameTime > this.timeLimit) this.gameOver = true;
       this.background.update();
       this.background.layer4.update;
-      this.player.update();
+      this.player.update(deltaTime);
       if (this.ammoTimer > this.ammoInterval) {
         if (this.ammo < this.maxAmmo) this.ammo++;
         this.ammoTimer = 0;
@@ -334,6 +363,8 @@ window.addEventListener(`load`, function () {
         enemy.update();
         if (this.checkCollision(this.player, enemy)) {
           enemy.markedForDeletion = true;
+          if ((enemy.type = "lucky")) this.player.enterPowerUp();
+          else this.score--;
         }
         this.player.projectiles.forEach((projectile) => {
           if (this.checkCollision(projectile, enemy)) {
